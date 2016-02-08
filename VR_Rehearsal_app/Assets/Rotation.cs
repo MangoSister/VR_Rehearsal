7da7d13777f9f1e2cfe3abf276162eb77facdb7f
myfriendkang <myfriendkang@gmail.com>
2016-02-07 22:24:54 -0500

    put rotation Scene before start play scene
    put rotation scene between UI scene and play scene

---
+++ VR_Rehearsal_app/Assets/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    private DownloadManager downloadManager
+    { get { return SceneManager.downloadManager; } }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            // SceneManager.LaunchPresentationScene(new PresentationInitParam("sc_present_0"));
+            Application.LoadLevel("sc_present_0");
+        }
+    }
+	
+}
