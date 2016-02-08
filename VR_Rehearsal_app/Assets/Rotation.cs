e0c6953504cba79608e68ba024f990b778455815
myfriendkang <myfriendkang@gmail.com>
2016-02-07 22:35:52 -0500

    fix rotation

--- VR_Rehearsal_app/Assets/Rotation.cs
+++ VR_Rehearsal_app/Assets/Rotation.cs
@@ -9,10 +9,10 @@ public class Rotation : MonoBehaviour {
     void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if(Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)
         {
             // SceneManager.LaunchPresentationScene(new PresentationInitParam("sc_present_0"));
             Application.LoadLevel("sc_present_0");
