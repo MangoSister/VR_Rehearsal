064c441eb6fe9f87191998bc78182e4b2059c947
MangoSister <yyp0502@gmail.com>
2016-02-07 20:51:43 -0500

    change orientation in main scene

--- VR_Rehearsal_app/Assets/SceneManagement/RoomCenter.cs
+++ VR_Rehearsal_app/Assets/SceneManagement/RoomCenter.cs
@@ -18,6 +18,9 @@ public class RoomCenter : MonoBehaviour
 
     void Start ()
     {
+#if UNITY_ANDROID
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+#endif 
         if (SceneManager.screenTransition != null)
             SceneManager.screenTransition.Fade(true, 1.0f);
 
