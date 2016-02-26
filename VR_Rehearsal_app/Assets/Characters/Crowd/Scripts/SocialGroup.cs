ce84ae769c869261898ffe1a6d49d5945c3a1e56
MangoSister <yyp0502@gmail.com>
2016-02-26 12:41:42 -0500

    migrate to Unity 5.3.3f1

--- VR_Rehearsal_app/Assets/Characters/Crowd/Scripts/SocialGroup.cs
+++ VR_Rehearsal_app/Assets/Characters/Crowd/Scripts/SocialGroup.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿/* HeatmapTracker.cs
+ * Yang Zhou, last modified on Feb 20, 2016
+ * HeatmapTracker stores the presenter's gaze data, generate heatmap based on the data,
+ * and performs gaze trajectory replay.
+ * Dependencies: need RoomCenter in VR scene, Cardboard post render object for editor ONLY screen overlay
+ * May implement interpolation in the future
+ */
+
+using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
