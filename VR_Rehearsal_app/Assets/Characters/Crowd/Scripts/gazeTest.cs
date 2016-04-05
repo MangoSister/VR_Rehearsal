55eea4ddb5f9bfb2071b4fd6e19221001dccce82
Byunghwan Lee <bh7819@gmail.com>
2016-04-04 23:00:48 -0400

    character updated

---
+++ VR_Rehearsal_app/Assets/Characters/Crowd/Scripts/gazeTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gazeTest : MonoBehaviour {
+
+	public Transform targetTransform;
+	public Transform HeadWrapper;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HeadWrapper.LookAt (targetTransform);
+	}
+}
