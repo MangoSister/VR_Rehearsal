5401594035357bfd9c4f1ac8506f977e6b6b9fc0
Xunchi Sun <xunchis@andrew.ad.cmu.edu>
2016-04-07 22:33:55 -0400

    Moving record audio import into subthread to solve a crash case

--- VR_Rehearsal_app/Assets/ReviewModule/ReplayController.cs
+++ VR_Rehearsal_app/Assets/ReviewModule/ReplayController.cs
@@ -14,6 +14,9 @@ public class ReplayController : MonoBehaviour {
     private PLAY_STATUS playStatus = PLAY_STATUS.STOP;
     private static List<KeyValuePair<float, int>> out_SlidesTransitionRecord;
     private static List<KeyValuePair<float, int>> out_PauseRecord;
+    private bool isProcessingAudio = false; //for the threaded job audio processing
+    private AudioProcessingJob pcmToUnityClip;
+    private float[] floatArray;
 
     [Header("Playback Slider Control")]
     public Slider playbackSlider;
@@ -35,6 +38,9 @@ public class ReplayController : MonoBehaviour {
     public GameObject prefabPauseMarker;
     [Header("Marker Control")]
     public Text testText;
+    [Header("Loading")]
+    public GameObject loadingText;
+    public GameObject groupReplayObjects;
 
     enum PLAY_STATUS
     {
@@ -141,141 +147,132 @@ public class ReplayController : MonoBehaviour {
         if (audioSource.clip != null)
             audioSource.clip = null;
 
-        byte[] byteArray = null;
+        pcmToUnityClip = new AudioProcessingJob();
         if ((PresentationData.out_RecordingFilePath != null) && (PresentationData.out_RecordingFilePath != ""))
-            try { byteArray = File.ReadAllBytes(PresentationData.out_RecordingFilePath); }
-            catch (FileNotFoundException e) { quarterTime.text = e.Message; }
+            pcmToUnityClip.setUpFile(PresentationData.out_RecordingFilePath);
         else
-        {
-            testText.text = PresentationData.out_RecordingFilePath + "\n";
-            byteArray = File.ReadAllBytes(@"C:\Users\xunchis\record.pcm"); //for testing
-        }
+            pcmToUnityClip.setUpFile(@"C:\Users\xunchis\record.pcm");
 
-        //byte > unity float
-        if (byteArray == null)
-        {
-            //UnityEngine.Debug.Log("File not found");
-            testText.text += "File not found\n";
-            return;
-        }
-
-        float[] floatArr = new float[byteArray.Length / 2 + 1];
-        int i;
-        short max = 0;
-        for (i = 0; i < byteArray.Length; i = i + 2)
-        {
-            //1 short = 2 bytes = 16 bit & in little endian
-            byte[] bytebuffer = new byte[2];
-            if (BitConverter.IsLittleEndian == true)
-            {
-                bytebuffer[0] = byteArray[i];
-                bytebuffer[1] = byteArray[i + 1];
-            }
-            else
-            {
-                bytebuffer[0] = byteArray[i + 1];
-                bytebuffer[1] = byteArray[i];
-            }
-
-            short valueS = BitConverter.ToInt16(bytebuffer, 0);
-            if (valueS > max) max = valueS;
-
-            //translate to -1.0~1.0f
-            float valueF = ((float)valueS * 20f) / 32768.0f;
-            floatArr[i / 2] = valueF;
-        }
-        floatArr[i / 2] = '\0';
-
-        //time stamp update
-        float totaltime = (floatArr.Length) / (float)FREQUENCY;
-        quarterTime.text = getTimeString(totaltime / 4.0f);
-        halfTime.text = getTimeString(totaltime / 2.0f);
-        softTime.text = getTimeString(totaltime * 0.75f);
-        endTime.text = getTimeString(totaltime);
-
-        AudioClip myClip = AudioClip.Create("record", floatArr.Length, 1, FREQUENCY, false, false);
-        myClip.SetData(floatArr, 0);
-        audioSource.clip = myClip;
-        //UnityEngine.Debug.Log("reset slider"); 
-        preventTrigger = true;
-        playbackSlider.direction = Slider.Direction.LeftToRight;
-        playbackSlider.minValue = 1;
-        playbackSlider.maxValue = audioSource.clip.length;
-        playbackSlider.value = 0;
-        preventTrigger = false;
-
-        if (PresentationData.out_SlidesTransitionRecord != null)
-            out_SlidesTransitionRecord = PresentationData.out_SlidesTransitionRecord;
-
-        //for testing
-        //out_SlidesTransitionRecord = new List<KeyValuePair<float, int>>();
-        //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(6.0f, 1));
-        //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(10.0f, 1));
-        //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(14.0f, 1));
-        //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(19.0f, 1));
-        //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(22.0f, 1));
-
-        //instantiate markers
-        if (isTransitionDisplay == true)
-        { 
-            foreach (KeyValuePair<float, int> transitionRecord in out_SlidesTransitionRecord)
-            {
-                var go = Instantiate(prefabTransitionMarker) as GameObject;
-                go.transform.parent = groupTransitionMarker.transform;
-
-                //calculate x coord
-                float xPos = -54f + (379f - (-54f)) * (transitionRecord.Key / totaltime);
-
-                go.GetComponent<RectTransform>().localPosition = new Vector3(xPos, -68.795f, 0f);
-            }
-        }
-
-        out_PauseRecord = new List<KeyValuePair<float, int>>();
-
-        if (PresentationData.out_FluencyRecord != null)
-        {
-            //long pause threshold is set to 3
-            int accumulatedTime = 0; //in ms!
-
-            for (int j = 0; j < PresentationData.out_FluencyRecord.Count; j++)
-            {
-                //check the length of current activity
-                int length = PresentationData.out_FluencyRecord[j].Value - accumulatedTime;
-
-                if (accumulatedTime != 0) //ignore the first pause
-                {
-                    testText.text += PresentationData.out_FluencyRecord[j].Key.ToString() + " " + length + "\n";
-                    if ((PresentationData.out_FluencyRecord[j].Key.ToString() == "False") && (length > 1500))
-                    {
-                        out_PauseRecord.Add(new KeyValuePair<float, int>((float)accumulatedTime / 1000.0f, 1));//not speaking
-                    }
-                }
-
-                accumulatedTime = PresentationData.out_FluencyRecord[j].Value;
-            }
-        }
-
-        //instantiate markers
-        if (isPauseDisplay == true)
-        {
-            foreach (KeyValuePair<float, int> pauseRecord in out_PauseRecord)
-            {
-                var go = Instantiate(prefabPauseMarker) as GameObject;
-                go.transform.parent = groupPauseMarker.transform;
-
-                //calculate x coord
-                float xPos = -101f + (331f - (-101f)) * (pauseRecord.Key / totaltime);
-
-                go.GetComponent<RectTransform>().localPosition = new Vector3(xPos, -63f, 0f);
-                //go.GetComponent<RectTransform>().localScale = new Vector3(2.0f, 2.0f, 2.0f);
-                go.GetComponent<PauseController>().time = pauseRecord.Key;
-            }
-        }
+        pcmToUnityClip.Start();
+        isProcessingAudio = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         preventTrigger = true;
+
+        if (isProcessingAudio == true)
+        {
+            loadingText.GetComponent<Text>().text = "Loading... (" + pcmToUnityClip.progress + "/" + pcmToUnityClip.endvalue+")";
+            
+            if (pcmToUnityClip.IsDone)
+            {
+                loadingText.SetActive(false);
+                
+                isProcessingAudio = false;
+                floatArray = pcmToUnityClip.getArray();
+                UnityEngine.Debug.Log(floatArray.Length);
+
+                //time stamp update
+                float totaltime = (floatArray.Length) / (float)FREQUENCY;
+                quarterTime.text = getTimeString(totaltime / 4.0f);
+                halfTime.text = getTimeString(totaltime / 2.0f);
+                softTime.text = getTimeString(totaltime * 0.75f);
+                endTime.text = getTimeString(totaltime);
+
+                AudioClip myClip = AudioClip.Create("record", floatArray.Length, 1, FREQUENCY, false, false);
+                myClip.SetData(floatArray, 0);
+                audioSource.clip = myClip;
+            
+                preventTrigger = true;
+                playbackSlider.direction = Slider.Direction.LeftToRight;
+                playbackSlider.minValue = 1;
+                playbackSlider.maxValue = audioSource.clip.length;
+                playbackSlider.value = 0;
+                preventTrigger = false;
+
+                if ((PresentationData.out_SlidesTransitionRecord != null) && (PresentationData.out_SlidesTransitionRecord.Count >= 0))
+                    out_SlidesTransitionRecord = PresentationData.out_SlidesTransitionRecord;
+                else
+                {
+                    //give it some test data
+                    out_SlidesTransitionRecord = new List<KeyValuePair<float, int>>();
+                    //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(1.0f, 1));
+                    //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(5.0f, 1));
+                    //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(10.0f, 1));
+                    //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(15.0f, 1));
+                    //out_SlidesTransitionRecord.Add(new KeyValuePair<float, int>(20.0f, 1));
+                }
+
+                //instantiate markers
+                if (isTransitionDisplay == true)
+                { 
+                    foreach (KeyValuePair<float, int> transitionRecord in out_SlidesTransitionRecord)
+                    {
+                        var go = Instantiate(prefabTransitionMarker) as GameObject;
+                        go.transform.parent = groupTransitionMarker.transform;
+
+                        //calculate x coord
+                        float xPos = -54f + (379f - (-54f)) * (transitionRecord.Key / totaltime);
+
+                        go.GetComponent<RectTransform>().localPosition = new Vector3(xPos, -68.795f, 0f);
+                    }
+                }
+
+                out_PauseRecord = new List<KeyValuePair<float, int>>();
+
+                if ((PresentationData.out_FluencyRecord != null) && (PresentationData.out_FluencyRecord.Count >= 0))
+                {
+                    //long pause threshold is set to 1.5s
+                    int accumulatedTime = 0; //in ms!
+
+                    for (int j = 0; j < PresentationData.out_FluencyRecord.Count; j++)
+                    {
+                        //check the length of current activity
+                        int length = PresentationData.out_FluencyRecord[j].Value - accumulatedTime;
+
+                        if (accumulatedTime != 0) //ignore the first pause
+                        {
+                            testText.text += PresentationData.out_FluencyRecord[j].Key.ToString() + " " + length + "\n";
+                            if ((PresentationData.out_FluencyRecord[j].Key.ToString() == "False") && (length > 1500))
+                            {
+                                out_PauseRecord.Add(new KeyValuePair<float, int>((float)accumulatedTime / 1000.0f, 1));//not speaking
+                            }
+                        }
+
+                        accumulatedTime = PresentationData.out_FluencyRecord[j].Value;
+                    }
+                }
+                else
+                {
+                    //give it some test data
+                    out_PauseRecord = new List<KeyValuePair<float, int>>();
+                    //out_PauseRecord.Add(new KeyValuePair<float, int>(1.0f, 1));
+                    //out_PauseRecord.Add(new KeyValuePair<float, int>(5.0f, 1));
+                    //out_PauseRecord.Add(new KeyValuePair<float, int>(10.0f, 1));
+                    //out_PauseRecord.Add(new KeyValuePair<float, int>(15.0f, 1));
+                    //out_PauseRecord.Add(new KeyValuePair<float, int>(20.0f, 1));
+                }
+
+                //instantiate markers
+                if (isPauseDisplay == true)
+                {
+                    foreach (KeyValuePair<float, int> pauseRecord in out_PauseRecord)
+                    {
+                        var go = Instantiate(prefabPauseMarker) as GameObject;
+                        go.transform.parent = groupPauseMarker.transform;
+
+                        //calculate x coord
+                        float xPos = -101f + (331f - (-101f)) * (pauseRecord.Key / totaltime);
+
+                        go.GetComponent<RectTransform>().localPosition = new Vector3(xPos, -63f, 0f);
+                        go.GetComponent<PauseController>().time = pauseRecord.Key;
+                    }
+                }
+
+                groupReplayObjects.SetActive(true);
+            }
+        }
 
         if (audioSource != null)
         {
@@ -298,4 +295,76 @@ public class ReplayController : MonoBehaviour {
         }
         preventTrigger = false;
 	}
+}
+
+public class AudioProcessingJob : ThreadedAudioJob
+{
+    private string filePath;
+    private const int FREQUENCY = 44100;
+    private float[] floatArray;
+    public int progress = 0;
+    public int endvalue = 0;
+
+    public void setUpFile(string filename)
+    {
+        filePath = filename;
+    }
+
+    public float[] getArray()
+    {
+        return floatArray;
+    }
+
+    protected override void ThreadFunction()
+    {
+        byte[] byteArray = null;
+        if ((PresentationData.out_RecordingFilePath != null) && (PresentationData.out_RecordingFilePath != ""))
+            try { 
+                byteArray = File.ReadAllBytes(PresentationData.out_RecordingFilePath); 
+            }
+            catch (FileNotFoundException e) { }
+        else
+        {
+            byteArray = File.ReadAllBytes(@"C:\Users\xunchis\record.pcm"); //for testing
+        }
+
+        //byte > unity float
+        if (byteArray == null)
+        {
+            //UnityEngine.Debug.Log("File not found");
+            return;
+        }
+        endvalue = byteArray.Length / 2;
+        floatArray = new float[byteArray.Length / 2 + 1];
+        int i;
+        short max = 0;
+        for (i = 0; i < byteArray.Length; i = i + 2)
+        {
+            //1 short = 2 bytes = 16 bit & in little endian
+            byte[] bytebuffer = new byte[2];
+            if (BitConverter.IsLittleEndian == true)
+            {
+                bytebuffer[0] = byteArray[i];
+                bytebuffer[1] = byteArray[i + 1];
+            }
+            else
+            {
+                bytebuffer[0] = byteArray[i + 1];
+                bytebuffer[1] = byteArray[i];
+            }
+
+            short valueS = BitConverter.ToInt16(bytebuffer, 0);
+            if (valueS > max) max = valueS;
+
+            //translate to -1.0~1.0f
+            float valueF = ((float)valueS * 20f) / 32768.0f;
+            floatArray[i / 2] = valueF;
+            if (i % 100000 == 0) progress = i/2;
+        }
+        floatArray[i / 2] = '\0';
+    }
+
+    protected override void OnFinished()
+    {
+    }
 }
