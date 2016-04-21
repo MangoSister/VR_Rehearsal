e40bbe76848c75df57965fa9a2ce77484fb9a661
Xunchi Sun <xunchis@andrew.ad.cmu.edu>
2016-04-21 16:05:35 -0400

    New slide thumbnail UI + legend on heatmap @ Review

---
+++ VR_Rehearsal_app/Assets/UIScript/ReviewModule/ThumbnailFrameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ThumbnailFrameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void SetFrameVisible(bool flag)
+    {
+        //gameObject.SetActive(flag);
+        if (flag == false)
+            gameObject.GetComponent<Transform>().localPosition = new Vector3(500, 0, 0);
+        else
+            gameObject.GetComponent<Transform>().localPosition = new Vector3(0, 0, 0);                
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
