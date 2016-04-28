ddf98269c7e1bda5d1183a3dc7e57276547f7e5a
Byunghwan Lee <bh7819@gmail.com>
2016-04-28 15:09:12 -0400

    d

--- plugInTest/app/src/main/java/com/example/byunghwl/plugintest/MainActivity.java
+++ plugInTest/app/src/main/java/com/example/byunghwl/plugintest/MainActivity.java
@@ -14,6 +14,7 @@ import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.StatFs;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import java.io.FileInputStream;
